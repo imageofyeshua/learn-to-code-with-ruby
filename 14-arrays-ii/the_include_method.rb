244967e84088339523f1a734ab68f8f2671597d4
Daniel Park <intheimageofyeshua@gmail.com>
2025-10-22 10:18:22 +0900

    The Include? Method

---
+++ 14-arrays-ii/the_include_method.rb
@@ -0,0 +1,9 @@
+# include? method - check for inclusion within the array
+
+p "action".include?("act")
+
+movie_genres = ["comedy", "action", "drama", "thriller", "crime"]
+
+p movie_genres.include?("drama")
+p movie_genres.include?("romance")
+p movie_genres.include?("Drama")
