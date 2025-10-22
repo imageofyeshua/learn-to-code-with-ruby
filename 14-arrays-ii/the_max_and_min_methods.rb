4f6071a7511230f49f99917364c4a3d9a50825d0
Daniel Park <intheimageofyeshua@gmail.com>
2025-10-22 10:26:41 +0900

    The Max and Min Methods

---
+++ 14-arrays-ii/the_max_and_min_methods.rb
@@ -0,0 +1,12 @@
+# max - greatest value in an array
+# min - smallest value in an array
+
+stock_prices = [432.12, 876.99, 75.12, 769.03]
+
+p stock_prices.max
+p stock_prices.min
+
+companies = ["Samsung", "Hyundai", "Apple", "Microsoft"]
+
+p companies.max
+p companies.min
